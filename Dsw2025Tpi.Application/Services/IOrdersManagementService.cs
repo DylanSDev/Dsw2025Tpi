f5a4b373ba4b1fc5f8888ca29d0953767213ad55
Celina-zts <celinazatososa@gmail.com>
2025-06-27 19:10:44 -0300

    Se agregaron las interfaces de servicio de manejo de productos y ordenes.

---
+++ Dsw2025Tpi.Application/Services/IOrdersManagementService.cs
@@ -0,0 +1,10 @@
+﻿using Dsw2025Tpi.Application.Dtos;
+
+
+namespace Dsw2025Tpi.Application.Services
+{
+        public interface IOrderManagementService
+        {
+            Task<OrderModel.OrderResponse> AddOrder(OrderModel.OrderRequest request);
+        }
+}
